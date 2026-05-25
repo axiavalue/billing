497aecc56569483d220eb1c5d02302b2ad95ecad
axiavalue <gg9736628@gmail.com>
2026-05-25 06:29:34 +0000

    remove old vanilla files

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-// Expose safe APIs to the renderer process
-contextBridge.exposeInMainWorld('electronAPI', {
-  getVersion: () => ipcRenderer.invoke('get-version'),
-  platform: process.platform
-});
